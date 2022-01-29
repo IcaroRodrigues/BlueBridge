1a8fe4ba221d2501dbcd92cd7e18165c2a910a23
Icaro Rodrigues <kevi_lothar@hotmail.com>
2022-01-29 02:49:17 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+
+import { Form } from './Components/Form'
+import { TodoList } from './Components/TodoList'
+
+import './App.css'
 
 function App() {
+  
+  const [inputText, setInputText] = useState('')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Icaro's Todo List</h1>
       </header>
+      <Form />
+      <TodoList />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

0db489b9a2115b2805d81be609eb9f70d052ec82
Icaro Rodrigues <kevi_lothar@hotmail.com>
2022-01-29 05:08:39 -0300

    Remove bug if input is empty, not change the state

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,14 +1,24 @@
-export const Form = ({ inputText, setInputText, todos, setTodos, setStatus }) => {
+export const Form = ({
+  inputText,
+  setInputText,
+  todos,
+  setTodos,
+  setStatus,
+}) => {
   const inputTextHandler = (e) => {
     setInputText(e.target.value)
   }
 
   const submitTodoHandler = (e) => {
     e.preventDefault()
-    setTodos([
-      ...todos,
-      { text: inputText, completed: false, id: Math.random() * 1000 },
-    ])
+
+    if (inputText !== '') {
+      setTodos([
+        ...todos,
+        { text: inputText, completed: false, id: Math.random() * 1000 },
+      ])
+    }
+
     setInputText('')
   }
 

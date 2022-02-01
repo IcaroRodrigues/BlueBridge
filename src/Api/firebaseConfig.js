acb23e7bd89098f8a9e35a1858940a319e26da14
Icaro Rodrigues <kevi_lothar@hotmail.com>
2022-02-01 01:09:56 -0300

    FEAT: add environment variables

--- src/Api/firebaseConfig.js
+++ src/Api/firebaseConfig.js
@@ -1,9 +1,5 @@
 export default {
-  apiKey: "AIzaSyAsDpkh02Lvhk3_-kNL-Xdb6vxK7Tm3pUs",
-  authDomain: "bluebridge-e5ba6.firebaseapp.com",
-  projectId: "bluebridge-e5ba6",
-  storageBucket: "bluebridge-e5ba6.appspot.com",
-  messagingSenderId: "29706397766",
-  appId: "1:29706397766:web:bfc4162aea04eda153c247",
-  measurementId: "G-SMGLVD7GEM"
-};
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+}
